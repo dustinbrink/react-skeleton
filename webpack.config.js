e0674829d1704243157bfe688b13e595fedfa84e
Dustin Brink <dustinbrink@gmail.com>
2016-07-16 14:11:14 -0600

    Webpack - move more plugins to parts.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,11 @@
 const webpack = require('webpack');
 const path = require('path');
-const HtmlPlugin = require('html-webpack-plugin');
 const merge = require('webpack-merge');
 const validate = require('webpack-validator');
 const parts = require('./lib/parts');
 
 const CONFIG = require('./package.json');
-const ENV_DEV =  process.env.DEV || true;
+
 
 const PATHS = {
 	build: path.resolve(__dirname, CONFIG.build.dest),
@@ -36,30 +35,8 @@ const common = {
 	output: {
 		path: PATHS.build,
 		filename: FILES.bundle
-	},
-	module : {
-		loaders : [
-			{
-				test : /\.jsx$/,
-				loader: 'babel-loader',
-				include : PATHS.app,
-				exclude: /(node_modules|bower_components)/,
-				query: {
-					cacheDirectory: true,
-					presets: ['es2015', 'react']
-				}
-			}
-		]
-	},
-	plugins: [
-		new HtmlPlugin({
-			title: CONFIG.name,
-			template: FILES.template,
-			minify: !ENV_DEV,
-			showErrors: ENV_DEV
-		})
-	]
-}
+	}
+};
 
 const bundleParts = {
 	name: 'vendor',
@@ -73,7 +50,8 @@ switch(process.env.npm_lifecycle_event) {
 
 	case 'build':
 	case 'stats':
-		process.env.NODE_ENV = 'production'
+		process.env.NODE_ENV = 'production';
+
 		webpackConfig = merge(
 			common,
 			{
@@ -86,15 +64,20 @@ switch(process.env.npm_lifecycle_event) {
 			},
 			parts.setVariable('process.env.NODE_ENV', process.env.NODE_ENV),
 			parts.clean(PATHS.build),
+			parts.babel(PATHS.app),
 			parts.extractBundle(bundleParts),
 			parts.minify(),
 			parts.extractCSS(FILES.styles),
-			parts.purifyCSS([PATHS.app])
+			parts.purifyCSS([PATHS.app]),
+			parts.html({
+				name: CONFIG.name,
+				template: FILES.template
+			})
 		);
 		break;
 
 	default: // Development
-		process.env.NODE_ENV = 'development'
+		process.env.NODE_ENV = 'development';
 
 		webpackConfig = merge(
 			common,
@@ -102,8 +85,13 @@ switch(process.env.npm_lifecycle_event) {
 				devtool: 'eval-source-map'
 			},
 			parts.setVariable('process.env.NODE_ENV', process.env.NODE_ENV),
+			parts.babel(PATHS.app),
 			parts.extractBundle(bundleParts),
 			parts.setupCSS(FILES.styles),
+			parts.html({
+				name: CONFIG.name,
+				template: FILES.template
+			}),
 			parts.devServer({
 				// Customize host/port here if needed
 				host: process.env.HOST,
